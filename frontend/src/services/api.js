79bc4def2d06a0ab673c2854a132983f0889e033
Mayur <ghulem66@gmail.com>
2026-03-08 08:06:27 +0530

    update api url

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const API = axios.create({
- baseURL:"http://localhost:5000/api"
+ baseURL:"https://leave-management-system-1-9wqx.onrender.com/api"
 })
 
 API.interceptors.request.use(req=>{
